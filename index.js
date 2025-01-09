cff31d9a298e8809149b3782c81b26689a320684
SrCatFr <momentobro803@gmail.com>
2025-01-09 15:41:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir archivos estáticos desde la carpeta public
+// Servir archivos estáticos
 app.use(express.static('public'));
 
 // Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rutas específicas para archivos estáticos
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'styles.css'));
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'script.js'));
+});
+
+// Ruta para imágenes
+app.get('/images/:folder/:file', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'images', req.params.folder, req.params.file));
 });
 
 // Para el despliegue en Vercel
